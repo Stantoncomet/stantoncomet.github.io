51fd310456aaabd466405925f7eb10a80f0e7491
Stantoncomet <96146851+Stantoncomet@users.noreply.github.com>
2024-02-01 22:44:58 -0800

    Update app.js

--- old-site/clues/01-sand/app.js
+++ old-site/clues/01-sand/app.js
@@ -1,24 +1,4 @@
-let squares; //what
-function convert() {
-    let csquares = "";
-    let acsquares = [];
-    squares.forEach(item => {
-        if (item.classList.contains('unactive'))
-            csquares += "0,";
-        else
-            csquares += "1,";
-    })
-    //csquares = csquares.slice(0, -1);
-    document.getElementById('piece').innerHTML = "["+csquares.slice(0, 15)+"],";
-    document.getElementById('piece').innerHTML += "<br>["+csquares.slice(16, 31)+"],";
-    document.getElementById('piece').innerHTML += "<br>["+csquares.slice(32, 47)+"],";
-    document.getElementById('piece').innerHTML += "<br>["+csquares.slice(48, 63)+"],";
-    document.getElementById('piece').innerHTML += "<br>["+csquares.slice(64, 79)+"],";
-    document.getElementById('piece').innerHTML += "<br>["+csquares.slice(80, 95)+"],";
-    document.getElementById('piece').innerHTML += "<br>["+csquares.slice(96, 111)+"],";
-    document.getElementById('piece').innerHTML += "<br>["+csquares.slice(112, 127)+"]";
-    console.log(acsquares);
-}
+
 
 document.addEventListener('DOMContentLoaded', () => {
     let grid = document.getElementById('grid');
@@ -47,3 +27,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 })
+let squares; //what
+function convert() {
+    let csquares = "";
+    let acsquares = [];
+    squares.forEach(item => {
+        if (item.classList.contains('unactive'))
+            csquares += "0,";
+        else
+            csquares += "1,";
+    })
+    //csquares = csquares.slice(0, -1);
+    document.getElementById('piece').innerHTML = "["+csquares.slice(0, 15)+"],";
+    document.getElementById('piece').innerHTML += "<br>["+csquares.slice(16, 31)+"],";
+    document.getElementById('piece').innerHTML += "<br>["+csquares.slice(32, 47)+"],";
+    document.getElementById('piece').innerHTML += "<br>["+csquares.slice(48, 63)+"],";
+    document.getElementById('piece').innerHTML += "<br>["+csquares.slice(64, 79)+"],";
+    document.getElementById('piece').innerHTML += "<br>["+csquares.slice(80, 95)+"],";
+    document.getElementById('piece').innerHTML += "<br>["+csquares.slice(96, 111)+"],";
+    document.getElementById('piece').innerHTML += "<br>["+csquares.slice(112, 127)+"]";
+    console.log(acsquares);
+}
