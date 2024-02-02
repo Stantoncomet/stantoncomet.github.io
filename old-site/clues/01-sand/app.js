56b362c999f79a17a4e58ae8caa26ca731de7f3a
Stantoncomet <olivercraftmoore@gmail.com>
2024-02-01 22:40:01 -0800

    convert to tetris peice button

--- old-site/clues/01-sand/app.js
+++ old-site/clues/01-sand/app.js
@@ -1,10 +1,32 @@
+let squares;
+function convert() {
+    let csquares = "";
+    let acsquares = [];
+    squares.forEach(item => {
+        if (item.classList.contains('unactive'))
+            csquares += "0,";
+        else
+            csquares += "1,";
+    })
+    //csquares = csquares.slice(0, -1);
+    document.getElementById('piece').innerHTML = "["+csquares.slice(0, 15)+"],";
+    document.getElementById('piece').innerHTML += "<br>["+csquares.slice(16, 31)+"],";
+    document.getElementById('piece').innerHTML += "<br>["+csquares.slice(32, 47)+"],";
+    document.getElementById('piece').innerHTML += "<br>["+csquares.slice(48, 63)+"],";
+    document.getElementById('piece').innerHTML += "<br>["+csquares.slice(64, 79)+"],";
+    document.getElementById('piece').innerHTML += "<br>["+csquares.slice(80, 95)+"],";
+    document.getElementById('piece').innerHTML += "<br>["+csquares.slice(96, 111)+"],";
+    document.getElementById('piece').innerHTML += "<br>["+csquares.slice(112, 127)+"]";
+    console.log(acsquares);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     let grid = document.getElementById('grid');
 
     for (let i = 0; i < 64; i++) {
         grid.appendChild(document.createElement('div'));
     }
-    let squares = Array.from(document.querySelectorAll('#grid div'));
+    squares = Array.from(document.querySelectorAll('#grid div'));
 
     squares.forEach(item => item.classList.add('unactive'));
 
@@ -19,6 +41,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     squares.forEach(item => item.addEventListener('mouseup', toggle));
+
+
 
 
 
