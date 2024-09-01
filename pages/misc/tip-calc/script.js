69c47d43b4da2f532f7026dd04587f79cd452d13
Stantoncomet <olivercraftmoore@gmail.com>
2024-09-01 00:19:46 -0700

    ooga booga

--- pages/misc/tip-calc/script.js
+++ pages/misc/tip-calc/script.js
@@ -1,21 +1,57 @@
-let answerBox = document.getElementById('answer');
-let totalDue = document.getElementById('total');
+let Receipt = document.getElementById('main');
+let AnswerBox = document.getElementById('answer');
+let TotalDue = document.getElementById('total');
+
+let can_tip = true;
 
 
-answerBox.addEventListener('keyup', e => {
-    let tip_ammount = answerBox.value;
-    let total = totalDue.innerText;
+document.addEventListener('keyup', e => {
+    if (!can_tip) return;
+
+    let tip_ammount = AnswerBox.innerText;
+    let total = TotalDue.innerText;
+
+    // console.log(e.key);
+    // console.log(e.key.match(/\d|\./));
+    if (e.key.match(/^(\d|\.)$/)) {
+        AnswerBox.innerText += e.key;
+    }
 
     if (e.code == "Enter" && !isNaN(tip_ammount)) {
         let tip = (tip_ammount/total*100).toFixed(1);
         console.log(tip+"%");
         document.getElementById('new-total').innerText = Number(tip_ammount)+Number(total);
         //document.getElementById('percent').innerText = tip+"%";
+
+        clearReceipt();
+        can_tip = false;
+    }
+
+    if (e.code == "Backspace") {
+        eraseTip();
+        can_tip = false;
     }
 });
 
+AnswerBox.addEventListener('animationend', e => {
+    console.log(e);
+    eraseTipEnd();
+    can_tip = true;
+})
 
+function clearReceipt() {
+    Receipt.style.animationName = 'leave-top';
+}
 
+function eraseTip() {
+    AnswerBox.style.animationName = 'erase-answer';
+    AnswerBox.style.color = 'rgba(0, 0, 0, 0.0)';
+}
+function eraseTipEnd() {
+    AnswerBox.style.color = 'rgba(0, 0, 0, 1.0)';
+    AnswerBox.style.animationName = '';
+    AnswerBox.innerHTML = '&nbsp;';
+}
 
 
 function checkAnswer(input) {
