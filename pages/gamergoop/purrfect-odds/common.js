2e1ddf163635e01a4dd4c386323fd75da549dad4
Stantoncomet <olivercraftmoore@gmail.com>
2025-02-06 23:16:24 -0800

    register home and patches

--- pages/gamergoop/purrfect-odds/common.js
+++ pages/gamergoop/purrfect-odds/common.js
@@ -1,3 +1,15 @@
+/***************************************************************************************************************/
+/**
+ * This common.js file is shared between subsites in purrfect odds. It should be the first script loaded on a site. 
+ * This top part is for accounts and data stuff.
+ */
+/***************************************************************************************************************/
+
+
+/**
+ * Fetches the latest user data from database server
+ * @returns All user data in JSON format
+ */
 async function getLatestData() {
 
     let data = await fetch('https://stantoncomet.gleeze.com:5000/odds_user_data')
@@ -5,6 +17,13 @@ async function getLatestData() {
         .catch(err => {console.log(err); return 0})
     return data;
 }
+/**
+ * Set the data of one resource of a user. Will probably just create a new resource if it doesn't exist
+ * @param {String} login_key 
+ * @param {String} resource 
+ * @param {String | Number} value 
+ * @returns Success or fail
+ */
 async function updateUserData(login_key, resource, value) {
     let success = await fetch('https://stantoncomet.gleeze.com:5000/odds_user_data', {
         mode: "cors",
@@ -23,12 +42,31 @@ async function updateUserData(login_key, resource, value) {
     return success;
 }
 
-
+/**
+ * Blinks a feedback message under the login field
+ * @param {String} message 
+ * @param {"error" | "success"} type 
+ */
 function loginFeedback(message, type="error") {
     let feedback = document.getElementById('login-feedback');
     feedback.innerText = message;
-    feedback.style.animationName = "flash-"+type;
 
+    // black magic that makes the animation restart when function is ran multiple times
+    feedback.style.animation = "none";
+    feedback.offsetHeight;
+    feedback.style.animation = null;
+
+    feedback.style.animationName = "flash-"+type;
     feedback.onanimationend = () => { feedback.style.animationName = ""; };
 }
 
+
+/*****************************************************************/
+/**
+ * Under this line is anything unrelated to accounts or user data.
+ */
+/*****************************************************************/
+
+function randItem(array) {
+    return array[Math.floor(Math.random()*array.length)];
+}
--- pages/gamergoop/purrfect-odds/register.js
+++ pages/gamergoop/purrfect-odds/register.js
@@ -1,4 +1,14 @@
+let wild_goose_chase = [
+    "Server tried to barrel roll... but it failed!",
+    "Shhhhh, the server is sleeping!",
+    "Server is out on lunch break!",
+    "[Gone fishing] -Server",
+    "Maybe try shaking things up a bit...",
+    "The server does NOT approve of those credentials!",
+    "Quack quack",
+    "Honk honk"
 
+]
 
 async function register() {
     let name = document.getElementById("name").value;
@@ -9,9 +19,9 @@ async function register() {
         return;
     }
     
-    let snapshot = getLatestData();
+    let snapshot = await getLatestData();
     if (snapshot[login_key]) {
-        loginFeedback("User already exists ...ssssshh!");
+        loginFeedback(randItem(wild_goose_chase));
         return;
     }
 
