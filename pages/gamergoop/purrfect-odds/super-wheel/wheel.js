11685899b421d002b078155c0727b266f16993de
Stantoncomet <olivercraftmoore@gmail.com>
2025-02-09 00:16:57 -0800

    increment

--- pages/gamergoop/purrfect-odds/super-wheel/wheel.js
+++ pages/gamergoop/purrfect-odds/super-wheel/wheel.js
@@ -24,7 +24,7 @@ async function spin() {
 
     let status = document.getElementById('fstatus');
     status.innerText = "Spinning...";
-    incimentBalance(current_login, Number(-200))
+    incrementBalance(current_login, Number(-200))
     snd_spin.load()
     snd_spin.play()
 
@@ -37,59 +37,59 @@ async function spin() {
         switch (true) {
             case result <= 1:
                 inputFeedback(`You win! (+Ⓟ25)`, input="flip", type="success");
-                incimentBalance(current_login, Number(25));
+                incrementBalance(current_login, Number(25));
                 break;
             case result >= 2 && result <= 3:
                 inputFeedback(`You win! (+Ⓟ50)`, input="flip", type="success");
-                incimentBalance(current_login, Number(50));
+                incrementBalance(current_login, Number(50));
                 break;
             case result == 4:
                 inputFeedback(`You win! (+Ⓟ100)`, input="flip", type="success");
-                incimentBalance(current_login, Number(100));
+                incrementBalance(current_login, Number(100));
                 break;
             case result >= 5 && result <= 7:
                 inputFeedback(`You win! (+Ⓟ200)`, input="flip", type="success");
-                incimentBalance(current_login, Number(200));
+                incrementBalance(current_login, Number(200));
                 break;
             case result == 8:
                 inputFeedback(`You win! (+Ⓟ500)`, input="flip", type="success");
-                incimentBalance(current_login, Number(500));
+                incrementBalance(current_login, Number(500));
                 break;
             case result == 9:
                 inputFeedback(`You win! (+Ⓟ1000)`, input="flip", type="success");
-                incimentBalance(current_login, Number(1000));
+                incrementBalance(current_login, Number(1000));
                 break;
             case result == 10:
                 inputFeedback(`You lose! (-Ⓟ50)`, input="flip", type="error");
-                incimentBalance(current_login, Number(-50));
+                incrementBalance(current_login, Number(-50));
                 break;
             case result >= 11 && result <= 12:
                 inputFeedback(`You lose! (-Ⓟ150)`, input="flip", type="error");
-                incimentBalance(current_login, Number(-150));
+                incrementBalance(current_login, Number(-150));
                 break;
             case result >= 13 && result <= 15:
                 inputFeedback(`You lose! (-Ⓟ200)`, input="flip", type="error");
-                incimentBalance(current_login, Number(-200));
+                incrementBalance(current_login, Number(-200));
                 break;
             case result == 16:
                 inputFeedback(`You lose! (-Ⓟ300)`, input="flip", type="error");
-                incimentBalance(current_login, Number(-300));
+                incrementBalance(current_login, Number(-300));
                 break;
             case result == 17:
                 inputFeedback(`You lose! (-Ⓟ400)`, input="flip", type="error");
-                incimentBalance(current_login, Number(-400));
+                incrementBalance(current_login, Number(-400));
                 break;
             case result == 18:
                 inputFeedback(`You lose! (-Ⓟ500)`, input="flip", type="error");
-                incimentBalance(current_login, Number(-500));
+                incrementBalance(current_login, Number(-500));
                 break;
             case result == 19:
                 inputFeedback(`Devastating loss! (Balance halved!)`, input="flip", type="success");
-                incimentBalance(current_login, Number(current_balance/2));
+                incrementBalance(current_login, Number(current_balance/2));
                 break;
             default: 
                 inputFeedback(`Something went wrong...`, input="flip", type="error");
-                incimentBalance(current_login, Number(100));
+                incrementBalance(current_login, Number(100));
         }
     }
 }
