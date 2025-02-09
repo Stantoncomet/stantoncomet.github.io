508afb344b6e95f0e047fe6021d0494cc6e2eae6
digifox <86581283+digifox-git@users.noreply.github.com>
2025-02-09 12:43:45 -0600

    Update home.js
    slightly improved random coin gain odds

--- pages/gamergoop/purrfect-odds/home.js
+++ pages/gamergoop/purrfect-odds/home.js
@@ -59,7 +59,7 @@ async function upMoneyRandom() {
     let snapshot = await fetchLatestData();
     let current_login = getCurrentLogin();
     let user_data = snapshot[current_login];
-    await updateUserData(current_login, 'balance', user_data.balance+(Math.floor(Math.random() * (20 - -20 + 1)) + -20));
+    await updateUserData(current_login, 'balance', user_data.balance+(Math.floor(Math.random() * (20 - -10 + 1)) + -10));
     updateHighScores();
 }
 
