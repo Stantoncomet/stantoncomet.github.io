d3f774aa4bd53fafdcfe72dd10ac502882b664fe
ThatCrazyManInASuit <57328239+ThatCrazyManInASuit@users.noreply.github.com>
2025-02-09 13:40:14 -0600

    plinko base

--- pages/gamergoop/purrfect-odds/plinko/plinko.js
+++ pages/gamergoop/purrfect-odds/plinko/plinko.js
@@ -1,4 +1,17 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+
+    const space = 50
+    function pegGen(n) {
+        for (let a = 1; a <= n; a++) {
+            let amt = a + 2
+            for (let b = 0; b < amt; b++) {
+                let peg = Bodies.circle(render.canvas.width / 2 - space - (space / 2 * (a - 1)) + space * b , render.canvas.height / 2 - (Math.sqrt(3) * space / 4) * n + (Math.sqrt(3) * space / 2) * a, 6, { isStatic: true });
+                Composite.add(engine.world, [peg])
+            }   
+        }
+    }
+
+    const renderSize = .7 * window.innerWidth;
+    console.log(renderSize)
     // module aliases
     var Engine = Matter.Engine,
     Render = Matter.Render,
@@ -11,17 +24,17 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     // create a renderer
     var render = Render.create({
-    element: document.body,
-    engine: engine
+        element: document.getElementById('renderer'),
+        engine: engine
     });
 
     // create two boxes and a ground
-    var boxA = Bodies.rectangle(400, 200, 80, 80);
-    var boxB = Bodies.rectangle(450, 50, 80, 80);
     var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
+    pegGen(10)
+
     // add all of the bodies to the world
-    Composite.add(engine.world, [boxA, boxB, ground]);
+    Composite.add(engine.world, [ground]);
 
     // run the renderer
     Render.run(render);
@@ -31,5 +44,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     // run the engine
     Runner.run(runner, engine);
-});
 
+
+function spawn() {
+    let ball = Bodies.circle(render.canvas.width/2, -100, 10);
+    Composite.add(engine.world, [ball]);
+}
