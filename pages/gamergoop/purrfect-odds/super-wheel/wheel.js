1b957cb4e1fb427d4e57eb36adb05a79377568df
Stantoncomet <olivercraftmoore@gmail.com>
2025-02-09 17:57:00 -0800

    increment!

--- pages/gamergoop/purrfect-odds/super-wheel/wheel.js
+++ pages/gamergoop/purrfect-odds/super-wheel/wheel.js
@@ -26,7 +26,7 @@ async function spin() {
 
     let status = document.getElementById('fstatus');
         status.innerText = "Spinning...";
-        incimentBalance(current_login, Number(-150))
+        incrementBalance(current_login, Number(-150))
         snd_spin.load()
         snd_spin.play()
 
@@ -41,67 +41,67 @@ async function spin() {
             // Expected Outcomes
             case result == 0:
                 inputFeedback(`You win! (+Ⓟ150)`, input="flip", type="success");
-                incimentBalance(current_login, Number(150));
+                incrementBalance(current_login, Number(150));
                 snd_success.load()
                 snd_success.play()
                 break;
             case result >= 1 && result <= 2:
                 inputFeedback(`You win! (+Ⓟ250)`, input="flip", type="success");
-                incimentBalance(current_login, Number(250));
+                incrementBalance(current_login, Number(250));
                 snd_success.load()
                 snd_success.play()
                 break;
             case result >= 3 && result <= 5:
                 inputFeedback(`You win! (+Ⓟ300)`, input="flip", type="success");
-                incimentBalance(current_login, Number(300));
+                incrementBalance(current_login, Number(300));
                 snd_success.load()
                 snd_success.play()
                 break;
             case result >= 6 && result <= 7:
                 inputFeedback(`You win! (+Ⓟ500)`, input="flip", type="success");
-                incimentBalance(current_login, Number(500));
+                incrementBalance(current_login, Number(500));
                 snd_success.load()
                 snd_success.play()
                 break;
             case result == 8:
                 inputFeedback(`Jackpot! (+Ⓟ2000)`, input="flip", type="success");
-                incimentBalance(current_login, Number(2000));
+                incrementBalance(current_login, Number(2000));
                 snd_jackpot.load()
                 snd_jackpot.play()
                 break;
             case result >= 9 && result <= 12:
                 inputFeedback(`You lose! (-Ⓟ200)`, input="flip", type="error");
-                incimentBalance(current_login, Number(-200));
+                incrementBalance(current_login, Number(-200));
                 snd_fail.load()
                 snd_fail.play()
                 break;
             case result >= 13 && result <= 16:
                 inputFeedback(`You lose! (-Ⓟ300)`, input="flip", type="error");
-                incimentBalance(current_login, Number(-300));
+                incrementBalance(current_login, Number(-300));
                 snd_fail.load()
                 snd_fail.play()
                 break;
             case result == 17:
                 inputFeedback(`You lose! (-Ⓟ400)`, input="flip", type="error");
-                incimentBalance(current_login, Number(-400));
+                incrementBalance(current_login, Number(-400));
                 snd_fail.load()
                 snd_fail.play()
                 break;
             case result == 18:
                 inputFeedback(`You lose! (-Ⓟ500)`, input="flip", type="error");
-                incimentBalance(current_login, Number(-500));
+                incrementBalance(current_login, Number(-500));
                 snd_fail.load()
                 snd_fail.play()
                 break;
             case result == 19:
                 inputFeedback(`Devastating loss! (-Ⓟ1000)`, input="flip", type="error");
-                incimentBalance(current_login, Number(-1000));
+                incrementBalance(current_login, Number(-1000));
                 snd_superfail.load()
                 snd_superfail.play()
                 break;
             default: 
                 inputFeedback(`Something went wrong... (Balance Refunded.)`, input="flip", type="error");
-                incimentBalance(current_login, Number(150));
+                incrementBalance(current_login, Number(150));
             // Strange Outcomes
             case result >= 20 && result <= 21:
                 inputFeedback(`Strange outcome! (Balance halved!)`, input="flip", type="error");
@@ -117,7 +117,7 @@ async function spin() {
                 break;
             case result == 23:
                 inputFeedback(`Strange outcome! (Balance × 2!)`, input="flip", type="success");
-                incimentBalance(current_login, Number(current_balance));
+                incrementBalance(current_login, Number(current_balance));
                 snd_strange.load()
                 snd_strange.play()
                 break;
