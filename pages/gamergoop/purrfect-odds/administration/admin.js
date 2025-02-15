5ec118c2da6f8d8d2073fb93e5ba194174edf060
digifox <digifox08@gmail.com>
2025-02-14 20:58:32 -0600

    changes to admin page

--- pages/gamergoop/purrfect-odds/administration/admin.js
+++ pages/gamergoop/purrfect-odds/administration/admin.js
@@ -4,6 +4,8 @@ let snd_money = new Audio("egg.wav");
 snd_money.volume = 0.5;
 let snd_error = new Audio("error.wav");
 snd_error.volume = 0.5;
+let snd_conf = new Audio("confirm.wav");
+snd_conf.volume = 0.5;
 
 async function deletesnd() {
     snd_delete.load();
@@ -16,41 +18,48 @@ async function moneysnd() {
     snd_money.play();
 }
 
-let passmin = "c0mjny82k1dj03d7"
-
-async function deleteUserData(login_key) {
-    
-    let success = await fetch('https://stantoncomet.gleeze.com:5000/odds_user_data', {
-        mode: "cors",
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            login_key: login_key,
-        })
-    })
-        .then(response => response.json())
-        .catch(err => console.log(err))
-    return success;
-}
-
 async function admin_setBalance(login_key, amount) {
-    if (login_key && amount && (document.getElementById('admin-password').value === passmin)) {
+    if (login_key && amount) {
         await inputFeedback("Balance updated!", input="login", type="success")
-        snd_money.load();
-        snd_money.play();
-        await updateUserData(login_key, "balance", amount);
+        snd_conf.load();
+        snd_conf.play();
+        await updateBalance(login_key, amount);
     }
     if (!login_key || !amount) {
         snd_error.load();
         snd_error.play(); 
         await inputFeedback("You must enter both fields!", input="login", type="error")
     }
+}
 
-    if (!(document.getElementById('admin-password').value === passmin)) {
+async function admin_fixbalance(login_key) {
+    if (login_key) {
+        await inputFeedback("Balance reset!", input="login", type="success")
+        snd_conf.load();
+        snd_conf.play();
+        await updateBalance(login_key, Number(1000));
+    }
+    if (!login_key || !modify_data) {
         snd_error.load();
         snd_error.play(); 
-        await inputFeedback("The admin password is incorrect!", input="login", type="error")
+        await inputFeedback("You must enter a password!", input="login", type="error")
     }
 }
+
+async function updateInfo(login_key) {
+    let snapshot = await fetchLatestData();
+    if (!snapshot) {
+        inputFeedback("An issue has occured out of your control");
+        return;
+    }
+    let modify_data = snapshot[login_key];
+    if (!modify_data) {
+        inputFeedback("User does not exist");
+        return;
+    }
+    document.getElementById("modify-user").innerText = modify_data.name+", $"+modify_data.balance;
+};
+
+setInterval(() => {
+    updateInfo(document.getElementById('modify-pass').value)
+}, 1000); // Runs every 1 second
